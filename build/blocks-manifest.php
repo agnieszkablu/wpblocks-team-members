4a4edc21fc417a05353d114e452e1c67b7ddc756
Agnieszka Sikora <sikora.au@gmail.com>
2026-04-12 18:12:46 +0200

    team member block and builds

--- build/blocks-manifest.php
+++ build/blocks-manifest.php
@@ -1,22 +1,55 @@
 <?php
 // This file is generated. Do not modify it manually.
 return array(
-	'boilerplate' => array(
+	'team-member' => array(
 		'$schema' => 'https://schemas.wp.org/trunk/block.json',
 		'apiVersion' => 3,
-		'name' => 'wpblocks/team-members',
+		'name' => 'wpblocks/team-member',
 		'version' => '0.1.0',
-		'title' => 'Boilerplate',
-		'category' => 'widgets',
-		'icon' => 'smiley',
-		'description' => 'Example block scaffolded with Create Block tool.',
+		'title' => 'Team member',
+		'parent' => array(
+			'wpblocks/team-members'
+		),
+		'category' => 'media',
+		'icon' => 'admin-users',
+		'description' => 'A block to display team member.',
+		'keywords' => array(
+			'team',
+			'member',
+			'staff',
+			'employee'
+		),
 		'example' => array(
-
+			
 		),
 		'supports' => array(
 			'html' => false
 		),
-		'textdomain' => 'boilerplate',
+		'textdomain' => 'team-member',
+		'editorScript' => 'file:./index.js'
+	),
+	'team-members' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'wpblocks/team-members',
+		'version' => '0.1.0',
+		'title' => 'Team members',
+		'category' => 'media',
+		'icon' => 'groups',
+		'description' => 'A block to display team members.',
+		'keywords' => array(
+			'team',
+			'members',
+			'staff',
+			'employees'
+		),
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'team-members',
 		'editorScript' => 'file:./index.js',
 		'editorStyle' => 'file:./index.css',
 		'style' => 'file:./style-index.css',
---
+++ build/team-members/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '9fc69dccb4f3374cfd23');
