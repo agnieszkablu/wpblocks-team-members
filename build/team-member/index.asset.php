eb9301a81e9e2d0d3d7734d1f5a2f681ace3cffb
Agnieszka Sikora <sikora.au@gmail.com>
2026-04-14 21:44:59 +0200

    base styles

--- build/team-member/index.asset.php
+++ build/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'db0460229d2edc43686a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1156c5b1cd2df2d66ff1');
--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '909c626c202366100148');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e3554776618bc6692624');
