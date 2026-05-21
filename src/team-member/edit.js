fbfd5c20ec67582bd5a85fd4b98edcf7579cf4ee
Agnieszka Sikora <sikora.au@gmail.com>
2026-05-21 17:46:33 +0200

    block save file, responsive image, handling errors while upload, blob url handle

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -1,9 +1,14 @@
+import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { useBlockProps, RichText, MediaPlaceholder } from '@wordpress/block-editor';
+import { isBlobURL, revokeBlobURL } from '@wordpress/blob';
+import { Spinner, withNotices } from '@wordpress/components';
 
-export default function Edit( { attributes, setAttributes } ) {
+function Edit( { attributes, setAttributes, noticeOperations, noticeUI } ) {
 
-	const { name, bio } = attributes;
+	const { name, bio, id, url, alt } = attributes;
+
+	const [blobUrl, setBlobUrl] = useState();
 
 	const onChangeName = ( newName ) => {
 		setAttributes( { name: newName } );
@@ -13,8 +18,60 @@ export default function Edit( { attributes, setAttributes } ) {
 		setAttributes( { bio: newBio } );
 	}
 
+	const onSelectImage = ( media ) => {
+		if ( ! media || ! media.url ) {
+			setAttributes( { url: undefined, alt: '', id: undefined } );
+			return;
+		}
+
+		setAttributes( { url: media.url, alt: media.alt, id: media.id } );
+	}
+
+	const onSelectURL = ( newURL ) => {
+		setAttributes( { url: newURL, alt: '', id: undefined } );
+	}
+
+	const onError = ( message ) => {
+		noticeOperations.removeAllNotices();
+		noticeOperations.createErrorNotice( message );
+	}
+
+	useEffect( () => {
+		if( !id && isBlobURL( url ) ) {
+			setAttributes( {
+				url: undefined,
+				alt: ''
+			} );
+		}
+	}, [] );
+
+	useEffect( () => {
+		if ( isBlobURL( url ) && url !== blobUrl ) {
+			setBlobUrl( url );
+		} else {
+			revokeBlobURL( blobUrl );
+			setBlobUrl( undefined );
+		}
+	}, [ url ] );
+
 	return (
 		<div { ...useBlockProps() }>
+			{url && <div className={`wp-block-blocks-team-member-img ${ isBlobURL( url ) ? 'is-loading' : '' }`}>
+				{isBlobURL( url ) && <Spinner />}
+				{url && !isBlobURL( url ) && (
+					<img src={ url } alt={ alt } id={ id } style={ { maxWidth: '100%' } } />
+				)}
+			</div> }
+			<MediaPlaceholder
+				icon="format-image"
+				onSelect={ onSelectImage }
+				onSelectURL={ onSelectURL }
+				allowedTypes={ [ 'image' ] }
+				accept='image/*'
+				onError={ onError }
+				disableMediaButtons={ url }
+				notices={ noticeUI }
+			/>
 			<RichText
 				tagName="h4"
 				value={ name }
@@ -32,3 +89,5 @@ export default function Edit( { attributes, setAttributes } ) {
 		</div>
 	);
 }
+
+export default withNotices( Edit );
--- src/team-member/save.js
+++ src/team-member/save.js
@@ -1,10 +1,15 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
 export default function Save( { attributes } ) {
-	const { name, bio } = attributes;
+	const { name, bio, url, alt, id } = attributes;
 
 	return (
 		<div {...useBlockProps.save()}>
+			{ url && (
+				<a href={ url }>
+					<img src={ url } alt={ alt } id={ id } className={ id ? `wp-image-${ id }` : null }/>
+				</a>
+			) }
 			<RichText.Content tagName="h4" value={ name } />
 			<RichText.Content tagName="p" value={ bio } />
 		</div>
